6aa04c43d594cbf530a5b8b4928f5d5546e1f414
szekedidavid <D.Szekedi@student.tudelft.nl>
2023-06-04 04:48:56 +0200

    uwu

--- MainCode.py
+++ MainCode.py
@@ -15,15 +15,24 @@ running = True
 white = (255, 255, 255)
 pg.init()
 x, y = 0, 0
+
+
+tsim = 0.0
+tstart = 0.001 * pg.time.get_ticks()
+dt = 0.1
+
+
 while running:
-    x += 0.01
-    y += 0.01
-    xs = int(x * ymax)
-    ys = ymax - int(y * ymax)
-    VRect.center = (xs, ys)
-    pg.draw.rect(surface, white, rect)
-    surface.blit(VSurface, VRect)
-    pg.display.flip()
-    time.sleep(0.1)
+    trun = 0.001 * pg.time.get_ticks() - tstart
+    if trun + dt >= tsim:
+        x += 0.01
+        y += 0.01
+        xs = int(x * ymax)
+        ys = ymax - int(y * ymax)
+        VRect.center = (xs, ys)
+        pg.draw.rect(surface, white, rect)
+        surface.blit(VSurface, VRect)
+        pg.display.flip()
+        time.sleep(0.1)
 
 pg.quit()
