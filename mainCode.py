f30d51f76ee4552a485a671494a4f073f20ded60
szekedidavid <D.Szekedi@student.tudelft.nl>
2023-06-04 17:27:20 +0200

    uwu

--- mainCode.py
+++ mainCode.py
@@ -66,33 +66,7 @@ while running:
         a = 0  # steady speed, either v0 or vmax
     v = v + a * dt  # increment velocity
 
-    """Change position"""
-    vx = v * np.cos(theta)
-    vy = v * np.sin(theta)
-    x += vx * dt  # relative coordinates
-    y += vy * dt
-    xs = int(x * yMax)  # surface coordinates
-    ys = yMax - int(y * yMax)
-
-    """Steer depending on location of mouse pointer"""
-    alpha = np.arctan2(ys - ym, xm - xs)  # get angle of pointer wrt V
-    if alpha < 0:  # convert -pi pi range of atan2 to 0 2pi
-        alpha += 2 * np.pi
-    if 2 * np.pi - theta + alpha < abs(alpha - theta):  # if angle has to go from under to above x axis
-        deltaTheta = 2 * np.pi - theta + alpha
-    elif 2 * np.pi + theta - alpha < abs(alpha - theta):  # if angle has to go from above to under the x axis
-        deltaTheta = - (2 * np.pi + theta - alpha)
-    else:
-        deltaTheta = alpha - theta  # if neither are needed
-    if abs(deltaTheta) > deltaThetaMax:  # limit rate of turn per time step
-        deltaTheta = np.sign(deltaTheta) * deltaThetaMax
-    theta += deltaTheta  # adjust angle by rate of turn
-
-    """Correct angle if out of bound"""
-    if theta >= 2 * np.pi:
-        theta -= 2 * np.pi
-    elif theta < 0:
-        theta += 2 * np.pi
+    x, y, xs, ys, theta = move(theta, x, y, xm, ym, deltaThetaMax, yMax, dt, v)  # rotate and move V
 
     """Display"""
     thetaDeg = int(np.degrees(theta))  # get theta in degrees
