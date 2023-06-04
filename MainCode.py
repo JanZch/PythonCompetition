94ad7e54231122d7eb3ccf4ddfd7b210cfa1d0de
szekedidavid <D.Szekedi@student.tudelft.nl>
2023-06-04 04:42:13 +0200

    uwu

--- MainCode.py
+++ MainCode.py
@@ -1,23 +1,29 @@
 import pygame as pg
+import time
 
 xmax, ymax = 1280, 720
 reso = (xmax, ymax)
 surface = pg.display.set_mode(reso)
+rect = surface.get_rect()
 
 VSurface = pg.image.load("FlyingV.png")
-VSurface = pg.transform.scale(VSurface, (50, 55))
+VSurface = pg.transform.smoothscale(VSurface, (73, 66))
 VRect = VSurface.get_rect()
 
 running = True
 
+white = (255, 255, 255)
 pg.init()
-
+x, y = 0, 0
 while running:
-    VRect.center = (500, 500)
+    x += 0.01
+    y += 0.01
+    xs = int(x * ymax)
+    ys = ymax - int(y * ymax)
+    VRect.center = (xs, ys)
+    pg.draw.rect(surface, white, rect)
     surface.blit(VSurface, VRect)
     pg.display.flip()
-
-# xs = int(x / 1.2 * xmax)
-# ys = ymax - int(y * ymax)
+    time.sleep(0.1)
 
 pg.quit()
